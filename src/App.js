1945853c83fbd2c7aedb84887fc2fa4cc70b374d
falamarcao <falamarcao@gmail.com>
2022-12-07 21:23:31 -0300

    160. Optimizing with useMemo()

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useMemo } from "react";
 import DemoOutput from "./Demo/DemoOutput";
 import Button from "./components/UI/Button";
 
 import "./App.css";
+import DemoList from "./Demo/DemoList";
 
 function App() {
   console.log("APP RUNING");
 
   const [showParagraph, setShowParagraph] = useState(false);
   const [allowToogle, setAllowToogle] = useState(false);
+  const [listTitle, setListTitle] = useState("My List");
 
   const handleAllowToggle = () => setAllowToogle(true);
 
@@ -18,13 +20,24 @@ function App() {
     }
   }, [allowToogle]);
 
+  const handleChangeTitle = useCallback(() => setListTitle("New Title"), []);
+
+  // Best Practices: only memoize [useMemo] data when it's computing expensive.
+  const items = useMemo(() => [5, 3, 1, 10, 9], []);
+
   return (
-    <div className="app">
-      <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
-      <Button onClick={handleAllowToggle}>Allow Toggle</Button>
-      <Button onClick={handleToogleParagraph}>Toggle Paragraph!</Button>
-    </div>
+    <>
+      <div className="app">
+        <h1>Hi there!</h1>
+        <DemoOutput show={showParagraph} />
+        <Button onClick={handleAllowToggle}>Allow Toggle</Button>
+        <Button onClick={handleToogleParagraph}>Toggle Paragraph!</Button>
+      </div>
+      <div className="app2">
+        <DemoList title={listTitle} items={items} />
+        <Button onClick={handleChangeTitle}>Change List Title</Button>
+      </div>
+    </>
   );
 }
 
